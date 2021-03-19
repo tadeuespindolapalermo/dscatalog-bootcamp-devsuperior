d07be5c569f293ef8a7f39f9acc86130b81dfcf1
Tadeu Espíndola Palermo <tadeupalermoti@gmail.com>
2021-03-19 01:08:19 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -4,6 +4,9 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -24,10 +28,21 @@ public class CategoryResource {
 
 	@Autowired
 	private CategoryService service;
-
+	
 	@GetMapping
 	public ResponseEntity<List<CategoryDTO>> findAll() {
 		return ResponseEntity.ok().body(service.findAll());
+	}
+
+	@GetMapping("/paged")
+	public ResponseEntity<Page<CategoryDTO>> findAllPaged(
+		@RequestParam(value = "page", defaultValue = "0") Integer page,
+		@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+		@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+		@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+	) {		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return ResponseEntity.ok().body(service.findAllPaged(pageRequest));
 	}
 	
 	@GetMapping(value = "/{id}")
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -8,6 +8,8 @@ import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,6 +28,11 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 	public List<CategoryDTO> findAll() {
 		return repository.findAll().stream().map(CategoryDTO::new).collect(Collectors.toList());
+	}
+	
+	@Transactional(readOnly = true)
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
+		return repository.findAll(pageRequest).map(CategoryDTO::new);
 	}
 
 	@Transactional(readOnly = true)
