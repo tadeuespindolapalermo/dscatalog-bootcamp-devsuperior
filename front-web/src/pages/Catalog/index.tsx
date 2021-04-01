56088c70b7baefeff65c92c9b974ddfc17cb264f
Tadeu Espíndola Palermo <tadeupalermoti@gmail.com>
2021-04-01 14:43:36 -0300

    Add product listing with backend

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,24 +1,45 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+//import axios from 'axios';
 import ProductCard from './components/ProductCard';
 import './styles.scss';
+import { makeRequest } from '../../core/utils/request';
+import { ProductsResponse } from '../../core/types/Product';
 
-const Catalog = () => (
-    <div className="catalog-container">
-        <h1 className="catalog-title">
-            Catálogo de Produtos
-        </h1>
-        <h1 className="catalog-products">
-            <Link to="/products/1"><ProductCard /></Link>
-            <Link to="/products/2"><ProductCard /></Link>
-            <Link to="/products/3"><ProductCard /></Link>
-            <Link to="/products/4"><ProductCard /></Link>
-            <Link to="/products/5"><ProductCard /></Link>
-            <Link to="/products/6"><ProductCard /></Link>
-            <Link to="/products/7"><ProductCard /></Link>
-            <Link to="/products/8"><ProductCard /></Link>
-            <Link to="/products/9"><ProductCard /></Link>
-        </h1>
-    </div>    
-);
+const Catalog = () => {
+
+    const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
+    
+    useEffect(() => {
+        /*fetch('http://localhost:3000/products')
+            .then(response => response.json())
+            .then(response => console.log(response));*/
+
+        /*axios('http://localhost:3000/products')            
+            .then(response => console.log(response));*/
+
+        const params = {
+            page: 0,
+            linesPerPage: 12
+        }
+        makeRequest({ url: '/products/paged', params })
+            .then(response => setProductsResponse(response.data));
+    }, []);
+
+    return (
+        <div className="catalog-container">
+            <h1 className="catalog-title">
+                Catálogo de Produtos
+            </h1>
+            <div className="catalog-products">
+               {productsResponse?.content.map(product => (
+                    <Link to={`/products/${product.id}`} key={product.id}>
+                        <ProductCard product={product}/>
+                    </Link> 
+               ))}              
+            </div>
+        </div>   
+    )
+};     
 
 export default Catalog;
